a193fccf779d8a22451b88763fe13f8a3bd2ec51
Adam Kearney <aklives@hotmail.com>
2019-05-19 20:49:05 -0400

    working

--- main.js
+++ main.js
@@ -1,8 +1,59 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const ERROR_MODAL = document.getElementById("modal")
 
 // Your JavaScript code goes here!
+const heartGlyphs = document.querySelectorAll(".like-glyph")
+const allHearts = Array.from(heartGlyphs)
+
+function throwError(error){
+    ERROR_MODAL.classList.remove('hidden');
+    setTimeout(function() {
+      ERROR_MODAL.className = ('hidden')
+    }, 5000)
+}
+
+
+function makeFull(target){
+  target.innerText = FULL_HEART
+  target.className = ('activated-heart')
+}
+
+function makeFetch(event) {
+  mimicServerCall()
+  .then(function() {
+    makeFull(event.target)})
+  .catch(response => {
+    console.log("ERROR ERROR")
+    throwError(response)
+  })
+}
+
+const heartClick = function(event) {
+  if(event.target.innerText === EMPTY_HEART) {
+    makeFetch(event);
+  } else {
+    event.target.innerText = EMPTY_HEART;
+    event.target.classList.remove("activated-heart");
+  }
+}
+
+allHearts.forEach(function (heart) {
+  heart.addEventListener("click", heartClick)
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
